3c9db7ae70a94af0954e3f5102b4887c1e226e5d
Epulvis <zuhrisaifuddin405@gmail.com>
2024-02-03 07:07:02 +0700

    hotfix/Popup ESC Event Fix

--- script/events.js
+++ script/events.js
@@ -1,44 +1,47 @@
 export default class PopupManager {
-  constructor(popupElement) {
-    this._popupElement = popupElement;
-  }
+	constructor(popupElement) {
+		this._popupElement = popupElement;
+	}
 
-  showPopup() {
-    this._popupElement.classList.add("popup_active");
-  }
+	showPopup() {
+		this._popupElement.classList.add("popup_active");
+	}
 
-  hidePopup() {
-    this._popupElement.classList.remove("popup_active");
-  }
+	hidePopup() {
+		this._popupElement.classList.remove("popup_active");
+		document.removeEventListener("keydown", function (event) {});
+	}
 }
 
 export function initializePopupEvents() {
-  document.addEventListener("DOMContentLoaded", function () {
-    const popups = document.querySelectorAll(".popup");
-    const popupManagers = Array.from(popups).map(popup => new PopupManager(popup));
+	document.addEventListener("DOMContentLoaded", function () {
+		const popups = document.querySelectorAll(".popup");
+		const popupManagers = Array.from(popups).map(
+			(popup) => new PopupManager(popup)
+		);
 
-    popups.forEach(function (popup, index) {
-      popup.addEventListener("click", function (event) {
-        if (event.target.classList.contains("popup")) {
-          popupManagers[index].hidePopup();
-        }
-      });
-    });
+		popups.forEach(function (popup, index) {
+			popup.addEventListener("click", function (event) {
+				if (event.target.classList.contains("popup")) {
+					popupManagers[index].hidePopup();
+				}
+			});
+		});
 
-    const closeButtons = document.querySelectorAll(".popup__button_type-close");
+		const closeButtons = document.querySelectorAll(".popup__button_type-close");
 
-    closeButtons.forEach(function (button, index) {
-      button.addEventListener("click", function () {
-        popupManagers[index].hidePopup(); 
-      });
-    });
+		closeButtons.forEach(function (button, index) {
+			button.addEventListener("click", function () {
+				popupManagers[index].hidePopup();
+			});
+		});
 
-    document.removeEventListener("keydown", function ( event) {
-      if (event.key === "Escape") {
-        popupManagers.forEach(function (popupManager) {
-          popupManager.hidePopup();
-        });
-      }
-    });
-  });
+		document.addEventListener("keydown", function (event) {
+			if (event.key === "Escape") {
+				popupManagers.forEach(function (popupManager) {
+					popupManager.hidePopup();
+				});
+			}
+		});
+	});
 }
