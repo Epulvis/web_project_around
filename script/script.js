77b282a955793598dbb0831ac5def84f41456fc5
Epulvis <zuhrisaifuddin405@gmail.com>
2024-02-08 17:26:02 +0700

    8.2.0/refactor code java script

--- script/script.js
+++ script/script.js
@@ -1,45 +1,79 @@
-import { editBtn, formInput, nameInput, jobInput, nameValue, jobValue, newCardBtn, newCardForm, popup, editFormModalWindow, cardFormModalWindow, data, cardsContainer } from "./utils.js";
 import FormValidator from "./FormValidator.js";
 import PopupManager, {
-  initializePopupEvents,
-  closePopupUsingEscButton,
-} from './events.js'; 
-import { addPlace } from "./cardFunctions.js";
+	initializePopupEvents,
+	closePopupUsingEscButton,
+} from "./utils.js";
+import Card from "./Card.js";
 
 // Elements
+const editBtn = document.querySelector(".profile__button_type-edit");
+const formInput = document.querySelector(".popup__form");
+const nameInput = document.getElementById("form-profile-username-input");
+const jobInput = document.getElementById("form-profile-job-input");
+const nameValue = document.querySelector(".profile__name");
+const jobValue = document.querySelector(".profile__job");
+const newCardBtn = document.querySelector(".profile__button_type-add");
+const newCardForm = document.querySelector(".popup__form_add-card");
+const popup = document.querySelector(".popup");
+const editFormModalWindow = document.querySelector(".edit-Profile");
+const cardFormModalWindow = document.querySelector(".new-place");
+const titleInput = document.querySelector(".popup__form-input_card-title");
+const urlInput = document.querySelector(".popup__form-input_type-url");
+const cardsContainer = document.querySelector(".cards__container");
 const popupManager = new PopupManager(popup);
+const popupAddPlace = new PopupManager(cardFormModalWindow);
+
+function addPlace(item) {
+	item.preventDefault();
+	const newCardData = {
+		name: titleInput.value,
+		link: urlInput.value,
+	};
+	const cardInstance = new Card(newCardData, "#card-template");
+	const newCardElement = cardInstance.generateCard();
+	cardsContainer.prepend(newCardElement);
+	popupAddPlace.hidePopup();
+	titleInput.value = "";
+	urlInput.value = "";
+}
 
 // Validation settings
 const enableValidation = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__form-input",
-  submitButtonSelector: ".popup__button_type-submit",
-  inactiveButtonClass: "popup__button_inactive",
-  inputErrorClass: "form__input_type_error",
-  errorClass: "form__input-error_active",
+	formSelector: ".popup__form",
+	inputSelector: ".popup__form-input",
+	submitButtonSelector: ".popup__button_type-submit",
+	inactiveButtonClass: "popup__button_inactive",
+	inputErrorClass: "form__input_type_error",
+	errorClass: "form__input-error_active",
 };
 
 // Validators
-const editFormValidator = new FormValidator(enableValidation, editFormModalWindow);
-const addFormValidator = new FormValidator(enableValidation, cardFormModalWindow);
+const editFormValidator = new FormValidator(
+	enableValidation,
+	editFormModalWindow
+);
+const addFormValidator = new FormValidator(
+	enableValidation,
+	cardFormModalWindow
+);
 
 // Functions
 function handleFormSubmit(event) {
-  event.preventDefault();
-  nameValue.textContent = nameInput.value;
-  jobValue.textContent = jobInput.value;
-  popupManager.hidePopup();
+	event.preventDefault();
+	nameValue.textContent = nameInput.value;
+	jobValue.textContent = jobInput.value;
+	popupManager.hidePopup();
 }
 
 function handleEditButtonClick() {
-  nameInput.value = nameValue.textContent;
-  jobInput.value = jobValue.textContent;
-  popupManager.showPopup();
+	nameInput.value = nameValue.textContent;
+	jobInput.value = jobValue.textContent;
+	popupManager.showPopup();
 }
 
 function handleNewCardButtonClick() {
-  cardFormModalWindow.classList.add("popup_active");
-  document.addEventListener('keydown', closePopupUsingEscButton)
+	cardFormModalWindow.classList.add("popup_active");
+	document.addEventListener("keydown", closePopupUsingEscButton);
 }
 
 // Initialize Validators
@@ -47,7 +81,7 @@ editFormValidator.enableValidation();
 addFormValidator.enableValidation();
 
 // Initialize Popup Events
-initializePopupEvents()
+initializePopupEvents();
 
 // Event Listeners
 formInput.addEventListener("submit", handleFormSubmit);
@@ -55,6 +89,6 @@ editBtn.addEventListener("click", handleEditButtonClick);
 newCardBtn.addEventListener("click", handleNewCardButtonClick);
 
 newCardForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  addPlace(event);
+	event.preventDefault();
+	addPlace(event);
 });
--- script/utils.js
+++ script/utils.js
@@ -1,50 +1,52 @@
-// elements
-export const editBtn = document.querySelector(".profile__button_type-edit");
-export const formInput = document.querySelector(".popup__form");
-export const nameInput = document.getElementById("form-profile-username-input");
-export const jobInput = document.getElementById("form-profile-job-input");
-export const nameValue = document.querySelector(".profile__name");
-export const jobValue = document.querySelector(".profile__job");
-export const newCardBtn = document.querySelector(".profile__button_type-add");
-export const newCardForm = document.querySelector(".popup__form_add-card");
-export const popup = document.querySelector(".popup");
-export const editFormModalWindow = document.querySelector(".edit-Profile");
-export const cardFormModalWindow = document.querySelector(".new-place");
-export const cardsContainer = document.querySelector(".cards__container");
-export const cardTemplate = document.getElementById("card-template");
-export const popupImages = document.querySelector(".popup-image");
-export const titleInput = document.querySelector(".popup__form-input_card-title");
-export const urlInput = document.querySelector(".popup__form-input_type-url");
+export default class PopupManager {
+	constructor(popupElement) {
+		this._popupElement = popupElement;
+	}
 
-export const data = [
-	{
-		name: "Lembah Yosemite",
-		link:
-			"https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
-	},
-	{
-		name: "Danau Louise",
-		link:
-			"https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
-	},
-	{
-		name: "Pegunungan Gundul",
-		link:
-			"https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
-	},
-	{
-		name: "Gunung Latemar",
-		link:
-			"https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
-	},
-	{
-		name: "Taman Nasional Vanoise",
-		link:
-			"https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
-	},
-	{
-		name: "Lago di Braies",
-		link:
-			"https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
-	},
-];
+	showPopup() {
+		this._popupElement.classList.add("popup_active");
+		document.addEventListener('keydown', closePopupUsingEscButton);
+	}
+
+	hidePopup() {
+		this._popupElement.classList.remove("popup_active");
+		document.removeEventListener('keydown', closePopupUsingEscButton);
+	}
+}
+
+export function initializePopupEvents() {
+	document.addEventListener("DOMContentLoaded", function () {
+		const popups = document.querySelectorAll(".popup");
+		const popupManagers = Array.from(popups).map(
+			(popup) => new PopupManager(popup)
+		);
+
+		popups.forEach(function (popup, index) {
+			popup.addEventListener("click", function (event) {
+				if (event.target.classList.contains("popup")) {
+					popupManagers[index].hidePopup();
+				}
+			});
+		});
+
+		const closeButtons = document.querySelectorAll(".popup__button_type-close");
+
+		closeButtons.forEach(function (button, index) {
+			button.addEventListener("click", function () {
+				popupManagers[index].hidePopup();
+			});
+		});
+	});
+}
+
+export function closePopupUsingEscButton(event) {
+    const popups = document.querySelectorAll('.popup');
+    const popupManagers = Array.from(popups).map(
+        (popup) => new PopupManager(popup),
+    );
+    if (event.key === 'Escape') {
+        popupManagers.forEach(function (popupManager) {
+            popupManager.hidePopup();
+        });
+    }
+} 
